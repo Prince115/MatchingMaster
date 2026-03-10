5615547ecd1adb33e1a433f453468477def14441
Deep Bhuva <93590755+DEEPBHUVA@users.noreply.github.com>
2026-03-10 22:57:50 +0530

    Remove all files

--- src/Inventory.Infrastructure/Entities/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using Microsoft.AspNetCore.Identity;
-
-namespace Inventory.Infrastructure.Entities;
-
-public class ApplicationUser : IdentityUser
-{
-    public string FullName { get; set; } = string.Empty;
-}
