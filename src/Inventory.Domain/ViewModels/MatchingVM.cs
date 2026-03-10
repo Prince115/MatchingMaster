efc18cf3447e95cf6cc7b6eaba76feebd34486ce
Deep Bhuva <93590755+DEEPBHUVA@users.noreply.github.com>
2026-03-10 20:56:16 +0530

    Initial commit

---
+++ src/Inventory.Domain/ViewModels/MatchingVM.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory.Domain.ViewModels
+{
+    public class MatchingVM
+    {
+        public int DesignId { get; set; }
+        public string? DesignNo { get; set; }
+        public int? Quality { get; set; }
+        public DateOnly Date { get; set; }
+        public int PartyId { get; set; }
+        public int Plates { get; set; }
+        public int Matching { get; set; }
+
+        [ValidateNever]   
+        public string PartyName { get; set; }
+
+        [ValidateNever]
+        public List<PlateRow> Rows { get; set; } = new();
+    }
+
+    public class PlateRow
+    {
+        [ValidateNever]
+        public int DesignPlateId { get; set; } 
+        public string? PlateName { get; set; }
+
+        [ValidateNever]
+        public string? PlateNo { get; set; }
+
+        [ValidateNever]
+        public List<MatchingCell> Matchings { get; set; } = new();
+    }
+
+    public class MatchingCell
+    {
+        [ValidateNever]
+        public int DesignMatchingId { get; set; }
+        
+        [ValidateNever]
+        public int MatchingNo { get; set; }
+        public string? Colour { get; set; }
+        public decimal? Quantity { get; set; }
+    }
+}
